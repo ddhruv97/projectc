d0a4bc127b18af2989265f87a5429064645ef95a
Dhruv <devulapallidhruv@gmail.com>
2016-11-02 02:06:32 -0700

    Added text field

--- submit.php
+++ submit.php
@@ -4,26 +4,26 @@ Name:
 <?php echo $_POST["contactname"]; ?><br>
 Major:
 <?php echo $_POST["major"]; ?><br>
+<?php echo htmlspecialchars($_POST["desc"]); ?><br>
 <?php
 $link = new mysqli(ini_get('mysql.default_host'), 
 		ini_get('mysql.default_user'),
-			 ini_get('mysqli.default_pw'),'projs');
+		ini_get('mysqli.default_pw'),
+		'projs');
 
 if ($link->connect_error) {
-die("Connection failed: " . $conn->connect_error);
+	die("Connection failed: " . $conn->connect_error);
+} else {
+	$command = $link->prepare("INSERT INTO ProjTable (name, major) VALUES (?,?)");
+	$command->bind_param("ss",$name, $major);
+	$name = $_POST["contactname"];
+	$major = $_POST["major"];
+	$command->execute();
+	$command->close();
+	$link->close();
+	echo "Added Record to db";
 }
-else {
-$command = $link->prepare("INSERT INTO ProjTable (name, major) VALUES (?,?)");
-$command->bind_param("ss",$name, $major);
-$name = $_POST["contactname"];
-$major = $_POST["major"];
-$command->execute();
-$command->close();
-$link->close();
-echo "Added Record to db";
-//<br>
-}
-?>
 
+?>
 <body>
 </html>
