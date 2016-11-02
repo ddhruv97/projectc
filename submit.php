8e9845bdc7430acf129a4927511a02bd1a09e2c1
root <root@ip-172-31-29-201.us-west-2.compute.internal>
2016-11-02 08:53:16 +0000

    Can submit now

--- submit.php
+++ submit.php
@@ -5,23 +5,25 @@ Name:
 Major:
 <?php echo $_POST["major"]; ?><br>
 <?php
-$conn = new mysqli(ini_get("mysql.default.host"),
-                     ini_get("mysql.default.user"),
-                     ini_get("mysql.default.password"),
-			"projs");
-if($conn->connect_error) {
+$link = new mysqli(ini_get('mysql.default_host'), 
+		ini_get('mysql.default_user'),
+			 ini_get('mysqli.default_pw'),'projs');
+
+if ($link->connect_error) {
 die("Connection failed: " . $conn->connect_error);
 }
-
-$stmt = $conn->prepare("INSERT INTO ProjTable (name, major) VALUES (?,?)");
-$stmt->bind_param("ss",$name $major);
-
+else {
+$command = $link->prepare("INSERT INTO ProjTable (name, major) VALUES (?,?)");
+$command->bind_param("ss",$name, $major);
 $name = $_POST["contactname"];
 $major = $_POST["major"];
-$stmt->execute();
-echo "Added to records";
-$stmt->close();
-$conn->close();
+$command->execute();
+$command->close();
+$link->close();
+echo "Added Record to db";
+//<br>
+}
 ?>
+
 <body>
 </html>
